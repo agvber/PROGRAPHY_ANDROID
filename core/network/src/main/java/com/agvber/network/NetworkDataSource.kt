7d994d75f68829fb5f38e480f8b1c1622ee73344
agvber <agvber12@gmail.com>
2024-01-30 13:33:34 +0900

    feat: get random photos api 추가

--- core/network/src/main/java/com/agvber/network/NetworkDataSource.kt
+++ core/network/src/main/java/com/agvber/network/NetworkDataSource.kt
@@ -1,7 +1,11 @@
 package com.agvber.network
 
+import androidx.annotation.IntRange
+import com.agvber.network.model.NetworkContentFilter
 import com.agvber.network.model.NetworkOrderBy
+import com.agvber.network.model.NetworkOrientation
 import com.agvber.network.model.PhotosResponse
+import com.agvber.network.model.RandomPhotosResponse
 
 interface NetworkDataSource {
 
@@ -10,4 +14,14 @@ interface NetworkDataSource {
         pageSize: Int = 10,
         orderBy: NetworkOrderBy = NetworkOrderBy.LATEST
     ): List<PhotosResponse>
+
+    suspend fun getRandomPhotos(
+        collections: List<String>? = null,
+        topics: List<String>? = null,
+        username: String? = null,
+        query: String? = null,
+        orientation: NetworkOrientation? = null,
+        contentFilter: NetworkContentFilter = NetworkContentFilter.LOW,
+        @IntRange(1, 30) count: Int = 1,
+    ): List<RandomPhotosResponse>
 }
--- core/network/src/main/java/com/agvber/network/retrofit/NetworkService.kt
+++ core/network/src/main/java/com/agvber/network/retrofit/NetworkService.kt
@@ -1,6 +1,8 @@
 package com.agvber.network.retrofit
 
+import androidx.annotation.IntRange
 import com.agvber.network.model.PhotosResponse
+import com.agvber.network.model.RandomPhotosResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -12,4 +14,15 @@ internal interface NetworkService {
         @Query("per_page") pageSize: Int, // Number of items per page. (Optional; default: 10)
         @Query("order_by") orderBy: String = "latest" // How to sort the photos. Optional. (Valid values: latest, oldest, popular; default: latest)
     ): List<PhotosResponse>
+
+    @GET("/photos/random")
+    suspend fun getRandomPhotos(
+        @Query("collections") collections: String? = null, // 	Public collection ID(‘s) to filter selection. If multiple, comma-separated
+        @Query("topics") topics: String? = null, // Public topic ID(‘s) to filter selection. If multiple, comma-separated
+        @Query("username") username: String? = null, // 	Limit selection to a single user.
+        @Query("query") query: String? = null, // Limit selection to photos matching a search term.
+        @Query("orientation") orientation: String? = null, // Filter by photo orientation. (Valid values: landscape, portrait, squarish)
+        @Query("content_filter") contentFilter: String = "low", // Limit results by content safety. Default: low. Valid values are low and high.
+        @Query("count") @IntRange(1, 30) count: Int = 1, // The number of photos to return. (Default: 1; max: 30)
+    ): List<RandomPhotosResponse>
 }
