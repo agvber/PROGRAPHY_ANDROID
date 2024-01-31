a4d56f9416326ce0243a50de80e317b42acde08c
agvber <agvber12@gmail.com>
2024-01-31 15:58:13 +0900

    chore: 라이브러리 추가
    compose-foundation,
    coil-compose

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -39,16 +39,20 @@ android {
 }
 
 dependencies {
+
     api(platform(libs.androidx.compose.bom))
     api(libs.androidx.compose.ui)
     api(libs.androidx.compose.ui.graphics)
     api(libs.androidx.compose.ui.tooling.preview)
     api(libs.androidx.compose.material3)
+    api(libs.androidx.compose.foundation)
 
     androidTestApi(platform(libs.androidx.compose.bom))
     androidTestApi(libs.androidx.compose.ui.test)
     debugApi(libs.androidx.compose.ui.tooling)
     debugApi(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.coil.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext)
